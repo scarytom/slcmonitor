696e771c7d2eee765576bc6d3e4c9f3173af7b93
Tom Denley <t.denley@cantab.net>
2009-01-31 16:02:18 +0000

    Attempt to create a Lender.

--- slcmonitor/src/slcmonitor.py
+++ slcmonitor/src/slcmonitor.py
@@ -3,6 +3,9 @@ import cgi
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
+from org.netmelody.slcmonitor.domain.lender import Lender
 
 class MainPage(webapp.RequestHandler):
   def get(self):
@@ -19,20 +22,41 @@ class MainPage(webapp.RequestHandler):
               <div><textarea name="content" rows="3" cols="60"></textarea></div>
               <div><input type="submit" value="Sign Guestbook"></div>
             </form>
+            <a href="lenders">Manage Lenders</a>
           </body>
         </html>""")
     else:
       self.redirect(users.create_login_url(self.request.uri))
 
-class Guestbook(webapp.RequestHandler):
-  def post(self):
-    self.response.out.write('<html><body>You wrote:<pre>')
-    self.response.out.write(cgi.escape(self.request.get('content')))
-    self.response.out.write('</pre></body></html>')
+class ManageLenders(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write('<html><body>')
+
+    lenders = db.GqlQuery("SELECT * FROM Lender ORDER BY name")
+
+    for lender in lenders:
+      self.response.out.write('Lender <b>%s</b>' % cgi.escape(lender.name))
+
+    # Write the submission form and the footer of the page
+    self.response.out.write("""
+          <form action="/addlender" method="post">
+            <div><textarea name="lenderName" rows="1" cols="60"></textarea></div>
+            <div><input type="submit" value="Add Lender"></div>
+          </form>
+        </body>
+      </html>""")
+
+class AddLender(webapp.RequestHandler):
+    def post(self):
+        lender = Lender()
+        lender.name = self.request.get('lenderName')
+        lender.put()
+        self.redirect('/lenders')
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/sign', Guestbook)],
+                                      ('/lenders', ManageLenders),
+                                      ('/addlender', AddLender)],
                                      debug=True)
 def main():
   run_wsgi_app(application)
