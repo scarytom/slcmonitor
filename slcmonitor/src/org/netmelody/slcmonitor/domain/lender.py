ca3a5ae7a6c4b1c03ef5fcbfe99e19808eb26857
Tom Denley <t.denley@cantab.net>
2009-02-01 17:41:49 +0000

    Attempt to implement currentRate method on Lender.

--- slcmonitor/src/org/netmelody/slcmonitor/domain/lender.py
+++ slcmonitor/src/org/netmelody/slcmonitor/domain/lender.py
@@ -1,5 +1,13 @@
 from google.appengine.ext import db
+#from ratechange import RateChange
 
 class Lender(db.Model):
     name = db.StringProperty()
     # List of rate changes
+    
+#    def currentRate(self):
+#        q = RateChange.all()
+#        q.filter(lender, self)
+#        q.order(-startDate)
+#        
+#        return q.fetch(1)[0]
