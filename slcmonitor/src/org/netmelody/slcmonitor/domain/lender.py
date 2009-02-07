0d73bc72a4c7bb8dbe7f4c61a717c0f5050e5309
Tom Denley <t.denley@cantab.net>
2009-02-07 11:00:56 +0000

    Implement retrieval of current rate for a lender.

--- slcmonitor/src/org/netmelody/slcmonitor/domain/lender.py
+++ slcmonitor/src/org/netmelody/slcmonitor/domain/lender.py
@@ -5,9 +5,16 @@ class Lender(db.Model):
     name = db.StringProperty()
     # List of rate changes
     
-#    def currentRate(self):
-#        q = RateChange.all()
-#        q.filter(lender, self)
-#        q.order(-startDate)
-#        
-#        return q.fetch(1)[0]
+    def currentRate(self):
+        latest = None
+        for rateChange in self.rateChanges:
+            if (latest == None) or (latest.startDate == None) or (rateChange.startDate > latest.startDate):
+                latest = rateChange
+        
+        if latest:
+            return latest.rate.value
+            
+            #q = RateChange.all()
+            #q.filter(lender, self)
+            #q.order(-startDate)
+            #return q.fetch(1)[0]
