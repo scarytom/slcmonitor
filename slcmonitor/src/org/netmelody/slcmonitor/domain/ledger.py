1f9b7be74a4826a19242025b5896bea0f0667d66
Tom Denley <t.denley@cantab.net>
2009-02-14 09:27:09 +0000

    Added getLoan method to Ledger

--- slcmonitor/src/org/netmelody/slcmonitor/domain/ledger.py
+++ slcmonitor/src/org/netmelody/slcmonitor/domain/ledger.py
@@ -1,4 +1,13 @@
 from google.appengine.ext import db
 
 class Ledger(db.Model):
-    None
+    
+    def getLoan(self):
+        if (self.loan_w):
+            return self.loan_w[0]
+        if (self.loan_r):
+            return self.loan_r[0]
+        if (self.loan_dr):
+            return self.loan_dr[0]
+        
+        
