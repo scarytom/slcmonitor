cb7e9c258c7335d210e7aad346ee5fd73afe98d2
Tom Denley <t.denley@cantab.net>
2009-02-15 20:20:23 +0000

    Changed Transaction class to be a polymorphic with three subclasses for Withdrawals, Distributed Repayments and Dated Repayments.

--- slcmonitor/src/org/netmelody/slcmonitor/domain/transaction.py
+++ slcmonitor/src/org/netmelody/slcmonitor/domain/transaction.py
@@ -1,7 +1,17 @@
 from ledger import Ledger
 from google.appengine.ext import db
+from google.appengine.ext.db import polymodel
 
-class Transaction(db.Model):
+class Transaction(polymodel.PolyModel):
     date = db.DateProperty()
     amount = db.IntegerProperty()
     ledger = db.ReferenceProperty(Ledger, collection_name='transactions')
+    
+class Withdrawal(Transaction):
+  None
+
+class DatedRepayment(Transaction):
+  None
+
+class DistributedRepayment(Transaction):
+  None
