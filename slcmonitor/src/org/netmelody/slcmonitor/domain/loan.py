8b461b9e8e669d5fddbc6f932cd22ba8efaef560
Tom Denley <none@none>
2009-02-18 23:57:31 +0000

    Added transaction management to loan.

--- slcmonitor/src/org/netmelody/slcmonitor/domain/loan.py
+++ slcmonitor/src/org/netmelody/slcmonitor/domain/loan.py
@@ -1,8 +1,26 @@
 from borrower import Borrower
 from lender import Lender
+from transaction import Withdrawal
+from transaction import DatedRepayment
+from transaction import DistributedRepayment
 from google.appengine.ext import db
 
 class Loan(db.Model):
     title = db.StringProperty()
     lender = db.ReferenceProperty(Lender, collection_name='loans')
     borrower = db.ReferenceProperty(Borrower, collection_name='loans')
+    
+    def makeWithdrawal(self, date, amount):
+        __createTransaction(Withdrawal(), date, amount)
+
+    def makeDatedRepayment(self, date, amount):
+        __createTransaction(DatedRepayment(), date, amount)
+        
+    def makeDistributedRepayment(self, date, amount):
+        __createTransaction(DistributedRepayment(), date, amount)
+        
+    def __createTransaction(self, transaction, date, amount):
+        transaction.amount = amount
+        transaction.date = date
+        transaction.loan = self
+        transaction.put()
